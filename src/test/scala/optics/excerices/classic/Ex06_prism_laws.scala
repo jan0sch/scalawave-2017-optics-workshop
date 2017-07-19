65f19af9dca150e06d108ce49115f3f8a5329355
Jens Grassel <jens@wegtam.com>
2017-07-19 22:07:26 +0200

    Exercise 06

--- src/test/scala/optics/excerices/classic/Ex06_prism_laws.scala
+++ src/test/scala/optics/excerices/classic/Ex06_prism_laws.scala
@@ -4,6 +4,8 @@ import optics.classic.Prism
 import org.specs2.Specification
 import org.specs2.cats.CatsEqMatcher
 
+import scala.util.Try
+
 class PrismLawsSpec extends Specification with CatsEqMatcher {
   override def is =
     s2"""
@@ -14,9 +16,18 @@ class PrismLawsSpec extends Specification with CatsEqMatcher {
 
     """
 
-  def forward = ???
+  def forward = {
+    val law: Double => Boolean = s => prism.getOption(prism.reverseGet(s)) === Some(s)
+    law(3.0)
+  }
 
-  def backward = ???
+  def backward = {
+    val law: String => Boolean = s => prism.getOption(s) match {
+      case None => true
+      case Some(d) => prism.reverseGet(d) === s
+    }
+    law("3.0")
+  }
 
-  def prism[S, A]: Prism[S, A] = ???
+  def prism: Prism[String, Double] = Prism[String, Double](s => Try(s.toDouble).toOption)(d => d.toString)
 }
